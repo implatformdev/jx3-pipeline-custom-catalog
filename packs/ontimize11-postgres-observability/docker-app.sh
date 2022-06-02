87e575249b78c9a29f3fdb0b82952ab68dc68717
angel.herce <angel.herce@imatia.com>
2022-06-02 09:17:57 +0200

    The "docker-compose.yaml" in buildpack "ontimize11-postgres-kafka" has been updated. - A script to launch the "app" service with docker-compose has been created. - The "docker-compose.yml" file so that the "app" service runs the script created has been updated.

---
+++ packs/ontimize11-postgres-observability/docker-app.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+if [ -z "$GRAFANA_ADMIN_API_KEY" ]
+then
+  echo "DOCKER-APP.SH -> GET API KEY"
+  key=$(curl -X POST -H "Content-Type: application/json" -d '{"name":"ontimize", "role": "Admin"}' http://admin:admin@grafana:3000/api/auth/keys | grep -oP '"key":"?\K[^"}]+')
+
+  if [ ! -z "$key" ]
+  then
+    echo "DOCKER-APP.SH -> SAVE API KEY"
+    export GRAFANA_ADMIN_API_KEY="$key"
+  fi
+fi
+
+echo "DOCKER-APP.SH -> CREATE DATASOURCE"
+curl -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "Authorization: Bearer $GRAFANA_ADMIN_API_KEY" -d '{"name":"Prometheus","type":"prometheus","url":"http://localhost:9090","access":"browser","basicAuth":false}"' http://grafana:3000/api/datasources
+
+echo "DOCKER-APP.SH -> COMPILE APP"
+mvn clean install -P compose
+
+echo "DOCKER-APP.SH -> RUN APP"
+java -jar obtest5-boot/target/obtest5-boot.jar --spring.profiles.active=compose
